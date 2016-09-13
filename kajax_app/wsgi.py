fff39df7b9c769d37c4969b29706d4caa11ae121
mateuszdargacz <mateuszdargacz@gmail.com>
2016-09-13 19:05:05 +0200

    models, admin and loading screen

--- kajax_app/wsgi.py
+++ kajax_app/wsgi.py
@@ -1,26 +1,9 @@
-"""
-WSGI config for sampleapp project.
-
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'kajax_app.settings'
+current_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(1, current_path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-
-# Apply WSGI middleware if necessary
 application = get_wsgi_application()
